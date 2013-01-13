6c67c0e1047871cb2fa99efa82942e277216fa65
freekus <freebsd99@gmail.com>
2013-01-13 17:54:35 -0500

    ok

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/test', test.show);
-app.get('/test', test.show);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
